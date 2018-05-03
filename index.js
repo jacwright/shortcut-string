b6b2f2621c085ac060d60dcc2ed94787871522a2
Jacob Wright <jacwright@gmail.com>
2018-05-03 10:43:02 -0600

    Updated to use event.key

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-var specialKeys = {
-  8: 'Backspace', 9: 'Tab', 13: 'Enter', 16: 'Shift', 17: 'Ctrl', 18: 'Alt', 19: 'Pause',
-  20: 'CapsLock', 27: 'Esc', 32: 'Space', 33: 'PageUp', 34: 'PageDown', 35: 'End', 36: 'Home',
-  37: 'Left', 38: 'Up', 39: 'Right', 40: 'Down', 45: 'Insert', 46: 'Del', 91: 'Meta',
-  96: '0', 97: '1', 98: '2', 99: '3', 100: '4', 101: '5', 102: '6', 103: '7',
-  104: '8', 105: '9', 106: '*', 107: '+', 109: '-', 110: '.', 111 : '/',
-  112: 'F1', 113: 'F2', 114: 'F3', 115: 'F4', 116: 'F5', 117: 'F6', 118: 'F7', 119: 'F8',
-  120: 'F9', 121: 'F10', 122: 'F11', 123: 'F12', 144: 'NumLock', 145: 'Scroll',
-  186: ';', 187: '=', 188: ',', 189: '-', 190: '.', 191: '/', 192: '`',
-  219: '[', 220: '\\', 221: ']', 222: "'", 224: 'Meta'
-};
+require('keyboardevent-key-polyfill').polyfill();
 
-var shiftChars = {
-  '`': '~', '1': '!', '2': '@', '3': '#', '4': '$', '5': '%', '6': '^', '7': '&', '8': '*', '9': '(', '0': ')',
-  '-': '_', '=': '+', '[': '{', ']': '}', '\\': '|', ';': ': ', '\'': '\"', ',': '<', '.': '>',  '/': '?'
-};
-
-var controlChars = {
-  Ctrl: true,
+var modifierKeys = {
+  Control: true,
   Meta: true,
   Shift: true,
   Alt: true
@@ -34,25 +19,20 @@ var controlChars = {
  * Shift+Down
  * Shift+F1
  *
- * Based off code from jQuery Hotkeys Plugin by John Resig
- * https://github.com/jeresig/jquery.hotkeys/
  */
 exports.fromEvent = function(event) {
   var shortcutArray = [];
+  var key = event.key;
 
   if (event.metaKey) shortcutArray.push('Cmd');
   if (event.ctrlKey) shortcutArray.push('Ctrl');
   if (event.altKey) shortcutArray.push('Alt');
   if (event.shiftKey) shortcutArray.push('Shift');
 
-  var character = specialKeys[event.which] || String.fromCharCode(event.which);
-
-  if (event.shiftKey && shiftChars.hasOwnProperty(character)) {
-    character = shiftChars[character];
-  }
-
-  if (!controlChars[character]) {
-    shortcutArray.push(character);
+  if (!modifierKeys[key]) {
+    // a and A, b and B, should be the same shortcut
+    if (key.length === 1) key = key.toUpperCase();
+    shortcutArray.push(key);
   }
 
   return shortcutArray.join('+');
